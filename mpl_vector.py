8fbf43c081803723362e77b079fb5a7b091e109e
spiltMilkis <azchen@uwaterloo.ca>
2025-01-15 15:46:54 -0800

    Update mpl_vector.py

--- mpl_vector.py
+++ mpl_vector.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import numpy as np
 
-x = input()
-y = [10, 20, 25, 30]
 
-plt.plot(x, y)
 
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.title("js a line plot")
+fig = plt.figure()
 
+ax = plt.axes(projection = '3d')
+z = 1
+x = 2
+y = 3
+
+ax.plot3D(x,y,z,'pink')
+ax.set_title('3D plot')
 plt.show()
 
 
