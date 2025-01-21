ece2291a23b164a47a045ebffffb5d3b7a9d8495
spiltMilkis <azchen@uwaterloo.ca>
2025-01-21 10:36:27 -0800

    Update mpl_vector.py

--- mpl_vector.py
+++ mpl_vector.py
@@ -1,18 +1,15 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
 import numpy as np
 
+x1 = int (input("x1?: "))
+y1 = int (input("y1?: "))
+V = np.array([x1,y1])
 
+fig = plt.subplots
+ax = plt.subplots
+ax.quiver(0, 0, V[0], V[1], angles = 'xy', scale_units = 'xy', scale = 1)
+ax.set_xlim([-2.2])
+ax.set_ylim([-2,2])
 
-fig = plt.figure()
-
-ax = plt.axes(projection = '3d')
-z = 1
-x = 2
-y = 3
-
-ax.plot3D(x,y,z,'pink')
-ax.set_title('3D plot')
+plt.grid()
 plt.show()
-
-
