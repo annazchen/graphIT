0cf96cbc6c39dcf65c9ff8d10c39af900ed1ac51
spiltMilkis <azchen@uwaterloo.ca>
2025-01-22 14:14:13 -0800

    usable image output thru mpl
    incorperated rulerMagv2 into mpl_vector, can display vector from magnitude values derived from rulerMagv2

--- mpl_vector.py
+++ mpl_vector.py
@@ -10,15 +10,13 @@ def vector_add(V1, V2):
     return Vtotal
 
 
-#converts any existing array with exclusively numbers into a numpy array
+#converts any existing array with EXCLUSIVELY NUMBERS into a numpy array
 def to_nparray(array):  
     V = np.array([])
-    for x in array:
-        V.append( float(array[x]))
-    return V    
+    return np.array(array, dtype = float)
 
 #adds vector to an existing plot according to 2 points, both w/ default val (0,0)   
-def create_vector(V1 = np.array([0, 0]), V2 = np.array([0, 0]), plt_color = 'r'): 
+def create_vector(V2 = np.array([0, 0]), V1 = np.array([0, 0]), plt_color = 'r'): 
     ax.quiver( V1[0], V1[1], V2[0], V2[1], angles = 'xy', scale_units = 'xy', scale = 1,  color  = plt_color)
 
 #shows a single vector NO IDEA IF THIS WORKS LOL
@@ -30,20 +28,20 @@ def show_vector(V0, x_low = -2, x_up = 2, y_low = -2, y_up = 2):
     plt.grid()
     plt.show()
     
-x1 = int (input("x1?: "))
-y1 = int (input("y1?: "))
-x2 = int(input("x2?: "))
-y2 = int(input("y2?: "))
+new_vMag = []
 
-V1 = np.array([x1, y1])
-V2 = np.array([x2, y1])
+for i in range(len(vMag)):
+    for j in range(len(vMag[i])):
+        new_vMag.append(vMag[i][j].strip('abcdefghijklmnopqrstuvwxyz')) 
+
+print(new_vMag)
+np_vMag = to_nparray(new_vMag)
+print(np_vMag)
 
 fig, ax = plt.subplots()
-ax.quiver(0, 0, V1[0], V1[1], angles = 'xy', scale_units = 'xy', scale = 1,  color  = 'r')
-ax.quiver(V1[0], V2[1], V2[0], V1[1], angles = 'xy', scale_units = 'xy', scale = 1,  color  = 'b')
-ax.quiver(0, 0, V1[0] + V2[0], V1[1] + V2[1], angles = 'xy', scale_units = 'xy', scale = 1,  color  = 'g')
-ax.set_xlim([-10,10])
-ax.set_ylim([-10,10])
+create_vector(np_vMag)
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
 
 plt.grid()
 plt.show()
